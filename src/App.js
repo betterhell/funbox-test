407c2ac37b59756467aacad731d9e915b85d132f
Dmitriy betterHell <skkogame@gmail.com>
2023-01-19 15:02:01 +0300

    style fixes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import {products} from "./data/products";
-import ProductCard from "./components/ProductCard/ProductCard"
+import { products } from "./data/products";
+import ProductCard from "./components/ProductCard/ProductCard";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <h1 className="App__title">Ты сегодня покормил кота?</h1>
-      <div className="products">
-        {products.map((product) =>
-            <ProductCard key={product.id} product={product} />
-        )}
+      <div className="App__products">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
       </div>
     </div>
   );
-}
+};
 
 export default App;
